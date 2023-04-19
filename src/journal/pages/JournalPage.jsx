21fe8d70ad2089ccd721f19436a7eae8448dc39e
Juan Jose <juanjosemartinez1999@outlook.com>
2023-04-18 20:21:40 -0500

    initcrud

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,13 +2,22 @@ import {IconButton } from "@mui/material";
 import {JournalLayout} from "../layout/JournalLayout.jsx";
 import {NoteView, NothingSelectedView} from "../views/index.js";
 import {AddOutlined} from "@mui/icons-material";
+import {useDispatch} from "react-redux";
+import {startNewNote} from "../../store/journal/index.js";
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch()
+
+    const onClickNewNote = () => {
+        dispatch( startNewNote() )
+    }
     return (
         <JournalLayout>
             <NothingSelectedView />
             {/*<NoteView/>*/}
             <IconButton size="large"
+                        onClick={ onClickNewNote }
                         sx={{
                             color: 'white',
                             backgroundColor: 'error.main',
---
+++ src/store/journal/thunks.js
@@ -0,0 +1,20 @@
+import { doc, collection, setDoc } from 'firebase/firestore/lite'
+import {FirebaseDB} from "../../firebase/config.js";
+
+export const startNewNote = () => {
+    return async ( dispatch, getState ) => {
+        const { uid } = getState().auth
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime()
+        }
+
+        const newDoc = doc( collection( FirebaseDB, `${uid}/journal/notes` ) );
+        const setDocResp = await setDoc( newDoc, newNote)
+
+        console.log(newDoc, setDocResp)
+
+
+    }
+}
